389e8a8947255f7e4292e2281e17edb00c3c257f
O Albert <emandari@gmail.com>
2024-08-23 18:04:32 +0300

    Change Vimeo to Youtube

--- src/partials/HeroHome.vue
+++ src/partials/HeroHome.vue
@@ -137,10 +137,13 @@
       >
         <div class="relative pb-9/16">
           <iframe
-            class="absolute w-full h-full"
-            src="https://player.vimeo.com/video/794280428"
-            title="Video"
-            allowFullScreen
+              class="absolute w-full h-full"
+              src="https://www.youtube-nocookie.com/embed/tm920xR44DE?si=0-PmnyZw3d9WSSEl"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin"
+              allowfullscreen
           ></iframe>
         </div>
       </Modal>
